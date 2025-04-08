f6f01468c72daead354ea8ff16d521f847523e04
AngelJimenezzz <147900694+AngelJimenezzz@users.noreply.github.com>
2025-04-08 10:37:42 -0600

    Agregar archivos de proyecto.

---
+++ Examen2da/Components/Models/Producto.cs
@@ -0,0 +1,10 @@
+﻿namespace TiendaDepartamental.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; } // Se usará para identificar el producto
+        public string Nombre { get; set; } = "";
+        public decimal Precio { get; set; }
+        public string Departamento { get; set; } = "";
+    }
+}
